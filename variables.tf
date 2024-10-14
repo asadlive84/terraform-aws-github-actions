8fb460c48ab97ef5e08c5eae12d4cc83cd664729
sohel <asadlive.sohel@gmail.com>
2024-10-14 19:28:55 +0000

    fix string

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
+# variables.tf
 
 variable "ssh_public_key" {
-    type = "string"
-    description = "value"
+  type        = string
+  description = "The public SSH key to be used for the EC2 instance"
 }
